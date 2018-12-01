f9f045d33305c0fc755553a7ef778cf50399e119
Mohit Gahlot <gahlot.mohit6@gmail.com>
2018-12-01 15:49:45 -0800

    Carasouel Layout v1

---
+++ Resources/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+     var scrollVal = 100;
+    setInterval(function () {
+        $(".hero-back-container").clearQueue();
+        $(".hero-back-container").animate({
+            left: -scrollVal+"%"
+        },1000);
+        scrollVal+=100;
+        if(scrollVal==500){
+            scrollVal=0;
+        }
+    },3000);
+
+});
---
+++ View/index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Marketplace</title>
+        <link rel="stylesheet" href="../Resources/css/main.css">
+        <link rel="stylesheet" href="../Resources/css/home.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="hero-section">
+                <div class="hero-back-container">
+                    <div class="hero-back" id="hero-back1"></div>
+                    <div class="hero-back" id="hero-back2"></div>
+                    <div class="hero-back" id="hero-back3"></div>
+                    <div class="hero-back" id="hero-back4"></div>
+                    <div class="hero-back" id="hero-back5"></div>
+                    <div class="hero-back" id="hero-back6"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="../Resources/js/main.js"></script>
+    <script src="../Resources/js/home.js"></script>
+</html>
