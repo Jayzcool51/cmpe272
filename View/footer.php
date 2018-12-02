8dbf7beed0ca09a2d1076a800c09067dc4ffc285
mayankdhingra02 <mayankdhingra02@gmail.com>
2018-12-02 03:52:07 -0800

    Testing

---
+++ View/footer.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Marketplace</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="../Resources/css/footer.css">
+	</head>
+    <body>
+        <footer>
+            <div id="right_footer">
+				<i class="fa fa-facebook-official" aria-hidden="true"></i>
+				jn
+			</div>
+        </footer>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="../Resources/js/main.js"></script>
+    <script src="../Resources/js/home.js"></script>
+</html>
