ebf1e15e9137ea626f3bd3319c916a7d8ae01570
mayankdhingra02 <mayankdhingra02@gmail.com>
2018-12-03 21:48:17 -0800

    cart_v1

---
+++ View/cart.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+    <title>Marketplace</title>
+    <link rel="stylesheet" href="../Resources/css/cart.css">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Open+Sans|Raleway|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Resources/css/animate.css">
+</head>
+<body>
+<div class="container">
+    <div class="container-image"></div>
+    <div class="navbar">
+        <div class="nav">Home</div>
+        <div class="nav">Products</div>
+        <div class="nav"></div>
+        <div class="nav">About Us</div>
+        <div class="nav">Contact</div>
+    </div>
+    <div class="hero-section">
+        <div class="products clearfix">
+            <div class="all_products clearfix">
+            <div class="product clearfix" >
+                <div class="product_img"></div>
+                <div class="product_desc_box">
+                    <div class="prod_name">
+                        Carlton London Women Rose Gold
+                    </div>
+                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ></div>
+
+                </div>
+                <div class="product_price">Price</div>
+
+            </div>
+            <div class="product clearfix">
+                <div class="product_img"></div>
+                <div class="product_desc_box">
+                    <div class="prod_name">
+                        Carlton London Women Rose Gold
+                    </div>
+                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ></div>
+
+                </div>
+                <div class="product_price">Price</div>
+
+            </div>
+            <div class="product clearfix">
+                <div class="product_img"></div>
+                <div class="product_desc_box">
+                    <div class="prod_name">
+                        Carlton London Women Rose Gold
+                    </div>
+                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ></div>
+
+                </div>
+                <div class="product_price">Price</div>
+
+            </div>
+            </div>
+            <div class="total_amount_box clearfix">
+                <div class="total">
+                    <div id="price_det"><b>PRICE DETAILS</b></div>
+                    <div>
+                        <div class="price_info_value"> bag tot</div>
+                        <div class="price_info">Bag Total</div>
+                    </div>
+                    <div>
+                        <div class="price_info_value">bag dis</div>
+                        <div class="price_info">Bag Discount</div>
+                    </div>
+                    <div>
+                        <div class="price_info_value">wst tax</div>
+                        <div class="price_info">Estimated Tax</div>
+                    </div>
+                    <div>
+                        <div class="price_info_value">dis cha</div>
+                        <div class="price_info">Delivery charges</div>
+
+                    </div>
+                </div>
+
+                <div class="total_amt_block">
+                    <div class="total_amt_value">1200</div>
+                    <div class="total_amt">Total amount</div>
+                </div>
+                    <div class="checkout_box"><b>PLACE ORDER</b></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+</html>
