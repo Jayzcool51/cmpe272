fbb28f8435e09f9703f440041088e467629cd354
Mohit Gahlot <gahlot.mohit6@gmail.com>
2018-12-05 03:55:38 -0800

    Analytics v3

--- View/Analytics.php
+++ View/Analytics.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../Resources/css/analytics.css">
+        <link href="https://fonts.googleapis.com/css?family=Cinzel|Open+Sans|Raleway|Roboto" rel="stylesheet">
     </head>
     <body>
         <div class="container">
@@ -17,15 +18,28 @@
                 <div class="nav">Contact</div>
             </div>
             <div id="contain">
-                <div id="canvas-holder" style="float:left;">
-                    <canvas id="bar-chart" width="400" height="450">
-                </canvas></div>
-                <div id="canvas-holder-keymsg" style="float:left;">
-                    <canvas id="doughnut-chart" width="500" height="450">
-                </canvas></div>
-                <div id="canvas-holder-keymsg" style="float:right;">
-                    <canvas id="bar-chart-horizontal" width="500" height="450">
-                </canvas></div>
+                <div class="top-section">
+                    <h4>Your Monthy Budget</h4>
+                    <canvas id="line-chart" height="300px"></canvas>
+                </div>
+                <div class="bottom-section clearfix">
+                    <div class="left-section">
+                        <h4>Purchase Category</h4>
+                        <canvas id="bar-chart" height="240px"></canvas>
+                    </div>
+                    <div class="right-section">
+                        <h4>Purchase Category</h4>
+                        <canvas id="bar-chart" height="240px"></canvas>
+                    </div>
+                </div>
+<!--                <div id="canvas-holder" style="float:left;">-->
+<!--                    </div>-->
+<!--                <div id="canvas-holder-keymsg" style="float:left;">-->
+<!--                    <canvas id="doughnut-chart" width="500" height="450">-->
+<!--                </canvas></div>-->
+<!--                <div id="canvas-holder-keymsg" style="float:right;">-->
+<!--                    <canvas id="bar-chart-horizontal" width="500" height="450">-->
+<!--                </canvas></div>-->
             </div>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
