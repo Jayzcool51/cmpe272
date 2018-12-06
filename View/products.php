a7ec8a2f440ba70b53c73940cd2500214a82861f
mayankdhingra02 <mayankdhingra02@gmail.com>
2018-12-06 00:36:33 -0800

    products footer

--- View/products.php
+++ View/products.php
@@ -2,6 +2,7 @@
 <head>
     <title>Marketplace</title>
     <link rel="stylesheet" href="../Resources/css/product.css">
+    <link rel="stylesheet" href="../Resources/css/footerv2.css">
     <link href="https://fonts.googleapis.com/css?family=Cinzel|Open+Sans|Raleway|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="../Resources/css/animate.css">
@@ -724,6 +725,39 @@
 
     </div>
 </div>
+<footer>
+    <div class="wrapper">
+        <div class="icon-row">
+            <div class="icon_box">
+                <a href="#"><i class="fab fa-facebook-square fa-lg"></i></a>
+                <a href="#"><i class="fab fa-instagram fa-lg"></i></a>
+                <a href="#"><i class="fab fa-twitter-square fa-lg"></i></a>
+                <a href="#"><i class="fab fa-youtube-square fa-lg"></i></a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="row"><a href=# class="anchor_row">About us</a></div>
+            <div class="row"><a href=# class="anchor_row">Media Center</a></div>
+            <div class="row"><a href=# class="anchor_row">Privacy</a></div>
+            <div class="row"><a href=# class="anchor_row">Contact Us</a></div>
+        </div>
+        <div class="box">
+            <div class="row"><a href=# class="anchor_row">Audio Description</a></div>
+            <div class="row"><a href=# class="anchor_row">Investor Relations</a></div>
+            <div class="row"><a href=# class="anchor_row">Legal Notices</a></div>
+        </div>
+        <div class="box">
+            <div class="row"><a href=# class="anchor_row">Help Center</a></div>
+            <div class="row"><a href=# class="anchor_row">Jobs</a></div>
+            <div class="row"><a href=# class="anchor_row">Cookie Preferences</a></div>
+        </div>
+        <div class="box_last">
+            <div class="row"><a href=# class="anchor_row">Gift Cards</a></div>
+            <div class="row"><a href=# class="anchor_row">Terms of Use</a></div>
+            <div class="row"><a href=# class="anchor_row">Corporate Information</a></div>
+        </div>
+    </div>
+</footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="../Resources/js/products.js"></script>
