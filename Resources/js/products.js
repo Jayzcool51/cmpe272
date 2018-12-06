0d8208a94dca0755b7c0097ff35ab16632186fef
Mohit Gahlot <gahlot.mohit6@gmail.com>
2018-12-06 04:27:36 -0800

    Removed bugs

--- Resources/js/products.js
+++ Resources/js/products.js
@@ -1,18 +1,25 @@
-val = 0;
-
 $(".right-icon").click(function () {
+    var element = $(this).parent().children().eq(2).children().eq(1);
+    val = element.data("pos");
     if(val!=-100){
         val-=100;
-        $(".most-searched-box").animate({
+        element.data("pos",val);
+        // $(".most-searched-box").animate({
+        //     left:val+"%"
+        // },1000);
+        $(element).animate({
             left:val+"%"
-        },1000)
+        },1000);
     }
 });
 
 $(".left-icon").click(function () {
+    var element = $(this).parent().children().eq(2).children().eq(1);
+    val = element.data("pos");
     if(val!=0){
         val+=100;
-        $(".most-searched-box").animate({
+        element.data("pos",val);
+        $(element).animate({
             left:val+"%"
         },1000)
     }
@@ -20,19 +27,22 @@ $(".left-icon").click(function () {
 
 clicked = false;
 $(".most-search-product-box").hover(function () {
-    $(".most-search-product-box").css({
-        width: "11%",
-        transform: "scale(1,1)"
-    });
-    clicked = false;
-    $(this).css({
-        transform: "scale(1,1.3)",
-        width: "15%"
-    });
-    $(".product-info").css({
-        display:"none"
-    });
-    $(this).children().eq(1).children().eq(0).fadeIn();
+    var check = $(this).parent().parent().parent().data("click");
+    if(!check){
+        $(".most-search-product-box").css({
+            width: "11.23%",
+            transform: "scale(1,1)"
+        });
+        clicked = false;
+        $(this).css({
+            transform: "scale(1,1.3)",
+            width: "15%"
+        });
+        $(".product-info").css({
+            display:"none"
+        });
+        $(this).children().eq(1).children().eq(0).fadeIn();
+    }
 },function () {
     if(!clicked){
         $(".most-search-product-box").css({
@@ -48,24 +58,43 @@ $(".most-search-product-box").hover(function () {
 
 
 $(".most-search-product-box").click(function () {
-    var check = $(this).parent().parent().data("click");
+    var check = $(this).parent().parent().parent().data("click");
     if(!check){
+        // $(".block").fadeIn();
         $(this).parent().parent().data("click",true);
+        $(this).parent().parent().parent().data("click",true);
         clicked = true;
-        $(".product-desc-section").animate({
-            height: 0
+        $(".product-desc-section").css({
+            height: 0,
+            border:0
         },100);
         $(this).css({
             transform: "scale(1,1.3)",
             width: "15%"
         });
-        var desc = $(this).parent().parent().children().eq(2);
+        var desc = $(this).parent().parent().parent().parent().children().eq(2);
         desc.css({
             border: "1px solid white"
         });
         desc.animate({
-            height: "300px"
+            height: "300px",
+            "margin-bottom": "30px"
         },300);
         $(this).children().eq(1).children().eq(0).fadeIn(200);
+    }
+    else{
+        $(".block").fadeOut();
+        $(this).parent().parent().data("click",false);
+        $(this).parent().parent().parent().data("click",false);
+        clicked = false;
+        $(".most-search-product-box").css({
+            width: "11.23%",
+            transform: "scale(1,1)"
+        });
+
+        $(".product-desc-section").animate({
+            height: 0,
+            border:0
+        },200);
     }
 });
