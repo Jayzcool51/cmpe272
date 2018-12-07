17e34c1b7ca86b10c33ab514e85d4b73b1a44ce2
mayankdhingra02 <mayankdhingra02@gmail.com>
2018-12-06 23:23:08 -0800

    searchv2

--- View/search.php
+++ View/search.php
@@ -5,6 +5,7 @@
         <link href="https://fonts.googleapis.com/css?family=Cinzel|Open+Sans|Raleway|Roboto" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <link rel="stylesheet" href="../Resources/css/search.css">
+
         <link rel="stylesheet" href="../Resources/css/footerv2.css">
     </head>
     <body>
@@ -29,6 +30,26 @@
                             <i class="fa fa-microphone" id="microphone" aria-hidden="true"></i>
                         </span>
                     </div>
+                    <div class="left_sort_box">
+                        <div class="left_box">
+                            <div class="left_box_heading">Sort By</div>
+                            <div class="left_box_row">Price: Low to High</div>
+                            <div class="left_box_row">Price: High to Low</div>
+                            <div class="left_box_row">Rating: Low to High</div>
+                            <div class="left_box_row">Rating: High to Low</div>
+
+                        </div>
+                        <div class="left_box">
+                            <div class="left_box_heading">Company</div>
+                            <div class="left_box_row">Mayank Dhingra</div>
+                            <div class="left_box_row">Mohit Gahlot</div>
+                            <div class="left_box_row">Dhruvil Patel</div>
+                            <div class="left_box_row">Jainam Sheth</div>
+                            <div class="left_box_row">Prabha Veerubhotla</div>
+                            <div class="left_box_row">Mohinish Daswani</div>
+                        </div>
+
+                    </div>
                         <div class="all_products clearfix">
 
                             <div class="product clearfix" >
@@ -39,17 +60,20 @@
                                     <div class="prod_name">
                                         Carlton London Women Rose Gold
                                     </div>
-                                    <div class="rating">rating</div>
-                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" >
+                                    <div class="rating"><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i></div>
+                                    <div class="product_description">This is the description of the product.This is the description of the product.</div>
+                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ><div></div>
                                     </div>
-
+                                    <div class="product_Website">
+                                        Website: www.mayankdhingra.com
+                                    </div>
                                 </div>
                                 <div class="product_desc_right clearfix">
                                     <div class="our_price">Our Price
                                         <div class="product_price">$15</div>
                                     </div>
-                                    <div class="cart">Add to Cart</div>
-                                    <div class="cart">Add to Wishlist</div>
+                                    <div class="cart"><i class="fa fa-shopping-bag" aria-hidden="true"></i>Add to Cart</div>
+                                    <div class="cart"><i class="fa fa-bookmark" aria-hidden="true"></i>Add to Wishlist</div>
                                 </div>
 
                             </div>
@@ -61,17 +85,20 @@
                                     <div class="prod_name">
                                         Carlton London Women Rose Gold
                                     </div>
-                                    <div class="rating">rating</div>
-                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" >
+                                    <div class="rating"><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i></div>
+                                    <div class="product_description">This is the description of the product.This is the description of the product.</div>
+                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ><div></div>
                                     </div>
-
+                                    <div class="product_Website">
+                                        Website: www.mayankdhingra.com
+                                    </div>
                                 </div>
                                 <div class="product_desc_right clearfix">
                                     <div class="our_price">Our Price
                                         <div class="product_price">$15</div>
                                     </div>
-                                    <div class="cart">Add to Cart</div>
-                                    <div class="cart">Add to Wishlist</div>
+                                    <div class="cart"><i class="fa fa-shopping-bag" aria-hidden="true"></i>Add to Cart</div>
+                                    <div class="cart"><i class="fa fa-bookmark" aria-hidden="true"></i>Add to Wishlist</div>
                                 </div>
 
                             </div>
@@ -83,17 +110,20 @@
                                     <div class="prod_name">
                                         Carlton London Women Rose Gold
                                     </div>
-                                    <div class="rating">rating</div>
-                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" >
+                                    <div class="rating"><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i><i class="fas fa-star fa-sm rating_star" ></i></div>
+                                    <div class="product_description">This is the description of the product.This is the description of the product.</div>
+                                    <div class="product_qty">Qty <input type="number" name="qty" value="1" width="12" ><div></div>
                                     </div>
-
+                                    <div class="product_Website">
+                                        Website: www.mayankdhingra.com
+                                    </div>
                                 </div>
                                 <div class="product_desc_right clearfix">
                                     <div class="our_price">Our Price
                                         <div class="product_price">$15</div>
                                     </div>
-                                    <div class="cart">Add to Cart</div>
-                                    <div class="cart">Add to Wishlist</div>
+                                    <div class="cart"><i class="fa fa-shopping-bag" aria-hidden="true"></i>Add to Cart</div>
+                                    <div class="cart"><i class="fa fa-bookmark" aria-hidden="true"></i>Add to Wishlist</div>
                                 </div>
 
                             </div>
