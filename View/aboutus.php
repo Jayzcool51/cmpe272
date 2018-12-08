01e83f250ba4b8fcce0bc9ff29d93cc99861c0c0
PrabhaVeerubhotla <vvlprabha@gmail.com>
2018-12-08 02:10:22 -0800

    adding images to aboutus
    Signed-off-by: PrabhaVeerubhotla <vvlprabha@gmail.com>

--- View/aboutus.php
+++ View/aboutus.php
@@ -28,29 +28,32 @@
                 <div class="web_name">Food</div>
             </div>
             <div class="profile" id="mohit">
+                <img src="../Resources/images/aboutus/image_Mohit.jpeg" style="width:100%">
                 <div class="Name">Mohit Gahlot</div>
                 <div class="web_name">Food</div>
             </div>
             <div class="profile" id="jainam">
+                <img src="../Resources/images/aboutus/image_Jainam.jpeg" style="width:300px;height:300px;">
                 <div class="Name">Jainam Sheth</div>
                 <div class="web_name">food</div>
             </div>
             <div class="profile" id="prabha">
-                <div class="Name">Prabha Veerubhotla</div>
-                <div class="web_name">dsad</div>
-            </div>
+              <img src="../Resources/images/aboutus/image_Prabha.jpg" style="width:200pxx;height:300px;">>
+                <div class="Name"> Prabha Veerubhotla</div>
+              </div>
             <div class="profile" id="dhruvil">
+              <img src="../Resources/images/aboutus/image_Dhruvil.jpeg" style="width:100%">
                 <div class="Name">Dhruvil Patel</div>
-                <div class="web_name">dassasad</div>
+                <!-- <div class="web_name">dassasad</div> -->
             </div>
             <div class="profile" id="mohinish">
                 <div class="Name">Mohinish Daswani</div>
                 <div class="web_name">dasas</div>
             </div>
-            <div class="profile" id="cmpe">
+            <!-- <div class="profile" id="cmpe">
                 <div class="Name">CMPE 272</div>
                 <div class="web_name">dasas</div>
-            </div>
+            </div> -->
         </div>
     </div>
     <div class="footer_class">
