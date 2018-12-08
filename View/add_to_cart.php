ccacefe9eecad1809ff49135f98d1c103afde82a
Dhruvil Patel <dhruvilpatel10fcb@gmail.com>
2018-12-08 10:33:15 -0800

    Session code added

--- View/add_to_cart.php
+++ View/add_to_cart.php
@@ -1,4 +1,7 @@
 <?php
+    session_start();
+?>
+<?php
     error_reporting(0);
     extract($_GET);
 
--- View/category_analytics.php
+++ View/category_analytics.php
@@ -1,4 +1,7 @@
 <?php
+    session_start();
+?>
+<?php
 //setting header to json
 header('Content-Type: application/json');
 
