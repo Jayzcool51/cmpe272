34e21e8b1bccb08d063da991be44229c93bbc7bc
Dhruvil Patel <dhruvilpatel10fcb@gmail.com>
2018-12-07 20:08:11 -0800

    Solved conn error in add_review and add_cart

--- View/add_review.php
+++ View/add_review.php
@@ -1,18 +1,24 @@
 <?php
+
+    extract($_GET);
+
     $servername = "13.56.13.38";
     $username = "admin";
     $password = "admin";
     $dbname = "gulliver";
+    
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
-    if ($conn->connect_error){
-        die("Connection failed: " . $conn->connect_error);
+    if(mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysql_connect_error();
     }
-    extract($_GET);
-    $user_id = 1;
-    $sql = "INSERT INTO `Rating` (`product_id`, `user_id`, `rating`, `review`) 
-    VALUES ('$product_id', '$user_id, '$rating', '$review')";
-    $result = mysqli_query($conn, $sql);
-    echo $result;
+    else {
+        $sql = "INSERT INTO `Rating` (`product_id`, `user_id`, `rating`, `review`) 
+        VALUES ('$product_id', '$user_id', '$rating', '$review')";
+        $result = mysqli_query($conn, $sql);
+        echo $result;
+        mysqli_close($conn);
+    }
 ?>
