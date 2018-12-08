9432e1597ef04e488ecf1b0169f9dd8cd15f257a
PrabhaVeerubhotla <vvlprabha@gmail.com>
2018-12-07 16:44:52 -0800

    analytics updated
    Signed-off-by: PrabhaVeerubhotla <vvlprabha@gmail.com>

---
+++ View/website_analytics.php
@@ -0,0 +1,40 @@
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '13.56.13.38');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_NAME', 'gulliver');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+	die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT p.website, SUM(p.price) FROM `Order` o
+INNER JOIN Product p
+ON p.product_id = o.product_id
+GROUP BY p.website");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
