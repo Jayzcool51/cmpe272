460a4ff8333762f870e73f16a6ec015a815232ec
Jayzcool51 <43188811+Jayzcool51@users.noreply.github.com>
2018-12-08 13:06:02 -0800

    fixes

--- View/auth_signup.php
+++ View/auth_signup.php
@@ -1,6 +1,6 @@
 <?php
 // Required if your environment does not handle autoloading
-require_once('../../twilio-php-master/Twilio/autoload.php');
+require_once('../twilio-php-master/Twilio/autoload.php');
 //random number generator
 
 // Use the REST API Client to make requests to the Twilio REST API
