8313370685e2a547e2b36a3201e780e9fafe3a53
Mohit Gahlot <gahlot.mohit6@gmail.com>
2018-12-08 20:03:30 -0800

    Payment Integrated

--- Resources/js/cart.js
+++ Resources/js/cart.js
@@ -1,3 +1,27 @@
 $(".checkout_box").click(function () {
-    console.log(arr);
+    // src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    // data-key="pk_test_pig1mf1SqmOCu7htLHMAAwmW"
+    // data-amount="999"
+    // data-name="laccros Marketplace"
+    // data-description="Payment Page"
+    // data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    // data-locale="auto">
+
+    all_val = $("#getValue").data("id");
+    arr = all_val.split("$");
+    len = arr.length;
+    total = arr[len-1];
+    newArr = arr.splice(1,len-2);
+    // data = {"data-key":"pk_test_pig1mf1SqmOCu7htLHMAAwmW","data-amount":arr[len-1], "data-name":"laccros Marketplace", "data-description":"Payment Page", "data-image":"../Resources/images/logo1.png", "data-locale":"auto"}
+    // console.log(data);
+    // $.ajax({
+    //     method: 'POST',
+    //     data: {'amount':arr[len-1],'items':newArr},
+    //     url: 'test.php'
+    // }).done(function (msg) {
+    //     console.log(msg);
+    // });
+    document.cookie = "total="+total;
+    document.cookie = "items="+newArr;
+    window.location.href = "test.php";
 });
--- View/checkout.php
+++ View/checkout.php
@@ -1,5 +1,9 @@
 <?php
-extract($_POST)
+session_start();
+?>
+<?php
+//print_r($_SESSION);
+//extract($_POST);
 require_once('../stripe-php-master/init.php');
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -8,23 +12,30 @@ require_once('../stripe-php-master/init.php');
 // Get the payment token ID submitted by the form:
 $token = $_POST['stripeToken'];
 $charge = \Stripe\Charge::create([
-    'amount' => 999,
+    'amount' => $_COOKIE['total'],
     'currency' => 'usd',
     'description' => 'Example charge',
     'source' => $token,
 ]);
-echo $charge;
+//echo $charge;
 if($charge){
-    echo $charge['id'].'<br>';
-echo $charge['amount'].'<br>';
+  //  echo $charge['id'].'<br>';
+//echo $charge['amount'].'<br>';
 #echo $charge['seller_message'].'<br>';
-#echo $charge['status'].'<br>';
-echo $charge['source']['last4'].'<br>';
-extract($_POST);
-$product_id=$_POST['prod_id'];
-$user_id=$_SESSION['user_id'];
+//echo $charge['status'].'<br>';
+//echo $charge['source']['last4'].'<br>';
+//extract($_POST);
+$product_id=$_COOKIE['items'];
+    $j=0;
+    for($i=0; $i<strlen($product_id);$i+=2){
+        $b[$j++] = $product_id[$i];
+    }
+    //$b = $product_id.explode(",");
+    //print_r($b);
+$user_id=$_SESSION["user_id"];
+  //  echo $_SESSION["user_id"];
 $total=$charge['amount'];
-$quantity=$_POST['quantity'];
+//$quantity=$_POST['quantity'];
 $cardlast=$charge['source']['last4'];
 $pay_id=$charge['id'];
 
@@ -41,20 +52,25 @@ if(mysqli_connect_errno())
     echo "Failed to connect to MySQL: " . mysql_connect_error();
 }
 else {
-    foreach($x as $product_id){
-    $sql = "INSERT INTO `Order` (`product_id`, `user_id`,`address`,`payment_id`, `payment_type`,`timestamp`,`count`,`total`, `quantity`,`cardlast`) 
-    VALUES ('$x', '$user_id',null,$pay_id,null,null,$total, '$quantity','$cardlast')";
+    foreach($b as $x){
+        //echo 'Here',$x;
+    $sql = "INSERT INTO `Order` (`product_id`, `user_id`,`payment_id`,`total`,`cardlast`) 
+    VALUES ($x, $user_id,'$pay_id',$total,'$cardlast')";
     $result = mysqli_query($conn, $sql);
-    echo $result;
+   // echo $result;
     }
 
     $sql = "DELETE FROM `Cart` WHERE user_id='$user_id'";
 
 if (mysqli_query($conn, $sql)) {
-echo "Record deleted successfully";
+//echo "Record deleted successfully";
 } else {
 echo "Error deleting record: " . mysqli_error($conn);
 }
 }
 mysqli_close($conn);}
+if($charge['status']==="succeeded") {
+    echo"<h2>Successfully added to your orders</h2>";
+    echo"<a href='home.php'>HOME</a>";
+}
 ?>
